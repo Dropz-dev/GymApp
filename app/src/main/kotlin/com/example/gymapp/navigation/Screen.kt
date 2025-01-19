7746a8f10f6ae17ffa7fe845fc06df9eeb80709b
Dropz-dev <tomsternberg@web.de>
2025-01-19 15:04:14 +0100

    Fix navigation setup and remove duplicate MainActivity

---
+++ app/src/main/kotlin/com/example/gymapp/navigation/Screen.kt
@@ -0,0 +1,9 @@
+package com.example.gymapp.navigation
+
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object ProgramList : Screen("program_list")
+    object TrainingDay : Screen("training_day/{programId}/{dayId}") {
+        fun createRoute(programId: Long, dayId: Long) = "training_day/$programId/$dayId"
+    }
+} 
